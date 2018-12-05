b4edbdaf0e9cb9a75b8fe75dc4b143d119b28e26
Pedro Nauck <pedronauck@gmail.com>
2018-12-05 13:04:36 -0200

    chore(docz): refac AsyncComponent to use hooks
    (cherry picked from commit 5b184bdf0aa5d2060591fd482f73bf08de461b46)

--- packages/docz/src/components/AsyncComponent.tsx
+++ packages/docz/src/components/AsyncComponent.tsx
@@ -1,57 +1,43 @@
 import * as React from 'react'
-import { ReactNode, ComponentType, Component } from 'react'
+import { useState, useEffect } from 'react'
+import { SFC, ComponentType } from 'react'
 
 import { isFn } from '../utils/helpers'
 
-interface Props {
+export interface AsyncComponentProps {
   as: ComponentType<any>
   getInitialData?: (props: any) => any
 }
 
-interface State {
-  loading: boolean
-  data: any
-  error: any
-}
+export const AsyncComponent: SFC<AsyncComponentProps> = ({
+  as: Comp,
+  getInitialData,
+  ...props
+}) => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState({})
 
-export class AsyncComponent extends Component<Props, State> {
-  public state = {
-    loading: true,
-    error: null,
-    data: {},
-  }
-
-  public componentDidMount(): void {
-    this.fetch()
-  }
-
-  public render(): ReactNode {
-    const { as: Comp, getInitialData, ...props } = this.props
-    const { data, loading, error } = this.state
-
-    return <Comp {...props} data={{ ...data, loading, error }} />
-  }
-
-  private async fetch(): Promise<void> {
-    const { getInitialData } = this.props
-
+  const fetch = async () => {
     if (getInitialData && isFn(getInitialData)) {
-      this.setState({ loading: true })
+      setLoading(true)
 
       try {
-        const data = await getInitialData(this.props)
-        this.setState({
-          data,
-          error: null,
-          loading: false,
-        })
+        const data = await getInitialData(props)
+        setData(data)
+        setError(null)
+        setLoading(false)
       } catch (error) {
-        this.setState({
-          error,
-          data: {},
-          loading: false,
-        })
+        setData({})
+        setError(null)
+        setLoading(false)
       }
     }
   }
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  return <Comp {...props} data={{ ...data, loading, error }} />
 }
