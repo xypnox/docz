4edbffedc3b55b4de9716aae17ed34d8afbe18ea
Pedro Nauck <pedronauck@gmail.com>
2018-12-05 13:43:15 -0200

    chore(docz): refac ScrollToTop to use hooks
    (cherry picked from commit eba46f50a780b09cfd4cb63a37224c6f1a93c522)

--- packages/docz/src/utils/ScrollToTop.tsx
+++ packages/docz/src/utils/ScrollToTop.tsx
@@ -1,18 +1,7 @@
-import { ReactNode, Component } from 'react'
-import { withRouter, RouteComponentProps } from 'react-router'
+import { useEffect, Children } from 'react'
+import { withRouter } from 'react-router'
 
-type ScrollToTopProps = RouteComponentProps<any>
-
-class ScrollToTopBase extends Component<ScrollToTopProps> {
-  public componentDidUpdate(prevProps: ScrollToTopProps): void {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
-    }
-  }
-
-  public render(): ReactNode {
-    return this.props.children
-  }
-}
-
-export const ScrollToTop: any = withRouter(ScrollToTopBase as any)
+export const ScrollToTop = withRouter(({ children, location }) => {
+  useEffect(() => window.scrollTo(0, 0), [location])
+  return Children.only(children)
+})
