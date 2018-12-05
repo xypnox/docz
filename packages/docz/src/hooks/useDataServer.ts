0f03306d3cfa10607edcfecc8ff1402d8068c3ce
Pedro Nauck <pedronauck@gmail.com>
2018-12-05 19:08:38 -0200

    chore(docz): refac data server hook
    (cherry picked from commit 4822e99f2159639b56da6729e4665259e63924bc)

--- packages/docz/src/hooks/useDataServer.ts
+++ packages/docz/src/hooks/useDataServer.ts
@@ -1,18 +1,21 @@
 import { useEffect } from 'react'
 import { state } from '../state'
 
+const updateState = (ev: any) => {
+  const { type, payload } = JSON.parse(ev.data)
+  const prop = type.startsWith('state.') && type.split('.')[1]
+
+  if (prop) {
+    state.set(state => ({ ...state, [prop]: payload }))
+  }
+}
+
 export const useDataServer = (url: string | undefined) => {
   useEffect(() => {
     if (!url) return
 
     const socket = new WebSocket(url)
-    socket.onmessage = (ev: any) => {
-      const { type, payload } = JSON.parse(ev.data)
-      const prop = type.startsWith('state.') && type.split('.')[1]
-
-      if (prop) {
-        state.set(state => ({ ...state, [prop]: payload }))
-      }
-    }
+    socket.onmessage = updateState
+    return () => socket.close()
   }, [])
 }
