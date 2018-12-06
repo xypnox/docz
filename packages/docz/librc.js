a31dd27a1c99e2e5aab73882c73658f365e7f3fe
Pedro Nauck <pedronauck@gmail.com>
2018-12-06 08:04:25 -0200

    chore: sort some lists
    (cherry picked from commit 389682f867a6d89afcff7b0fc6f335d26bd11d7b)

--- packages/docz/librc.js
+++ packages/docz/librc.js
@@ -2,18 +2,18 @@ const pkg = require('./package.json')
 
 module.exports = {
   external: Object.keys(pkg.dependencies).concat([
+    '@mdx-js/tag/dist/mdx-provider',
+    '@sindresorhus/slugify',
     '~db',
     '~imports',
-    '@sindresorhus/slugify',
-    '@mdx-js/tag/dist/mdx-provider',
     'array-sort',
     'capitalize',
     'create-react-context',
     'lodash/fp',
     'react-imported-component',
-    'react-router',
     'react-router-dom',
     'react-router-hash-link',
+    'react-router',
     'ulid',
   ]),
 }
--- packages/remark-docz/src/types.d.ts
+++ packages/remark-docz/src/types.d.ts
@@ -1,2 +1,2 @@
-declare module 'unist-util-visit'
 declare module 'unist-util-remove'
+declare module 'unist-util-visit'
